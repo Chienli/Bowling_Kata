873be937c0ecdee47650a5a3c6c8dd215e375e73
Guy Lin <xup6ru03xu4@gmail.com>
2019-07-27 14:26:53 +0800

    Rolls_all_0_return_0 pass

--- Bowling_Kata/BowlingKata.cs
+++ Bowling_Kata/BowlingKata.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 
 namespace Bowling_Kata
 {
     public class BowlingKata
     {
+        public int Score(ArrayList result)
+        {
+            return 0;
+        }
     }
 }
--- Bowling_Kata/BowlingKataTest.cs
+++ Bowling_Kata/BowlingKataTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bowling_Kata
@@ -6,5 +7,26 @@ namespace Bowling_Kata
     [TestClass]
     public class BowlingKataTest
     {
+        private readonly BowlingKata _bowlingKata = new BowlingKata();
+
+        [TestMethod]
+        public void Rolls_all_0_return_0()
+        {
+            var result = new ArrayList();
+            result = Roll(0, result, 20);
+
+            var actual = _bowlingKata.Score(result);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        private static ArrayList Roll(int pins, ArrayList result, int times = 1)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                result.Add(pins);
+            }
+            return result;
+        }
     }
 }
